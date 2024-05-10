995623bb72c4d9d733e6d2727d75587a20c4bb46
Luiz Ricardo Filho <filho.ricardo13@gmail.com>
2024-05-10 16:13:06 -0300

    Subindo dashboard

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,9 +1,12 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginController {
 	@FXML
@@ -11,12 +14,30 @@ public class LoginController {
 	
 	@FXML
 	private TextField password;
+	public Stage primaryStage;
 	
 	public void entrar() {
 		System.out.println("Login realizado com sucesso");
 		
 		if(email.getText().equals("admin") && password.getText().equals("admin")) {
 			System.out.println("Realizando login com acesso ADMIN");
+			
+			try {
+				FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/dashboard.fxml"));
+				
+				Parent root = loader.load();
+				Scene scene = new Scene(root,12000,800);
+				
+				Stage currentStage = (Stage) email.getScene().getWindow();
+				
+				currentStage.close();
+				
+				currentStage.setScene(scene);
+				currentStage.setTitle("Dashboard");
+				currentStage.show();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		} else {
 			mensagemErro();
 		}
